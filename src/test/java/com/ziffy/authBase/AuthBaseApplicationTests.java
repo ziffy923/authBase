34581edfc236decf372b5132e8ed8866e62870f7
ziffy923 <mahaprasad923@gmail.com>
2022-10-08 17:09:34 +0530

    First working version with CORS disabled

---
+++ src/test/java/com/ziffy/authBase/AuthBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziffy.authBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
